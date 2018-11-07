05ce6e694b6398576acb920f7599df0461db9512
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-07 22:44:56 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -40,3 +40,20 @@ export default class MovieBrowser extends React.Component {
     );
   }
 }
+
+export default class SomeComponent extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className="some-component-special-class">{child}</div>
+      );
+    });
+
+    return (
+      <div className="some-component">
+        <p>This component has {React.Children.count(this.props.children)} children.</p>
+        {childrenWithWrapperDiv}
+      </div>
+    );
+  }
+}
